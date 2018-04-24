b236aef4100c3fe997674d198c389f9b1307561d
Robert de Bock <robert@meinit.nl>
2018-04-24 17:18:34 +0200

    Variables in terraform...

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,11 +1,11 @@
-# Set the variable value in *.tfvars file
-# or using -var="do_token=..." CLI option
 variable "do_token" {}
 
-# Configure the DigitalOcean Provider
 provider "digitalocean" {
   token = "${var.do_token}"
 }
+
+variable "cloudflare_email" {}
+variable "cloudflare_token" {}
 
 provider "cloudflare" {
   email = "${var.cloudflare_email}"
