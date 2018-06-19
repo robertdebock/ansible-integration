f9df5d668f099d51acbdbb72465574bc6debf48d
Robert de Bock <robert@meinit.nl>
2018-06-19 17:24:07 +0200

    Build the machine9 ...

--- terraform/tooling.tf
+++ terraform/tooling.tf
@@ -2,7 +2,7 @@ resource "digitalocean_droplet" "machine8" {
   image    = "centos-7-x64"
   name     = "machine8.meinit.nl"
   region   = "ams3"
-  size     = "2gb"
+  size     = "1gb"
   ssh_keys = ["${digitalocean_ssh_key.default.id}"]
 }
 
@@ -10,6 +10,22 @@ resource "cloudflare_record" "machine8" {
   domain = "meinit.nl"
   name   = "machine8"
   value  = "${digitalocean_droplet.machine8.ipv4_address}"
+  type   = "A"
+  ttl    = 300
+}
+
+resource "digitalocean_droplet" "machine9" {
+  image    = "centos-7-x64"
+  name     = "machine9.meinit.nl"
+  region   = "ams3"
+  size     = "2gb"
+  ssh_keys = ["${digitalocean_ssh_key.default.id}"]
+}
+
+resource "cloudflare_record" "machine9" {
+  domain = "meinit.nl"
+  name   = "machine9"
+  value  = "${digitalocean_droplet.machine9.ipv4_address}"
   type   = "A"
   ttl    = 300
 }
