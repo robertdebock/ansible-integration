383ea2ec606a37d82e0ed2abdfcf438c726a94a7
Robert de Bock <robert@meinit.nl>
2018-09-19 07:09:16 +0200

    Cleaner and simpler.

--- terraform/machines.tf
+++ terraform/machines.tf
@@ -1,31 +1,31 @@
-resource "digitalocean_droplet" "machine1" {
+resource "digitalocean_droplet" "server1" {
   image    = "centos-7-x64"
-  name     = "machine1.meinit.nl"
+  name     = "server1.meinit.nl"
   region   = "ams3"
   size     = "2gb"
   ssh_keys = ["${digitalocean_ssh_key.default.id}"]
 }
 
-resource "cloudflare_record" "machine1" {
+resource "cloudflare_record" "server1" {
   domain = "meinit.nl"
-  name   = "machine1"
-  value  = "${digitalocean_droplet.machine1.ipv4_address}"
+  name   = "server1"
+  value  = "${digitalocean_droplet.server1.ipv4_address}"
   type   = "A"
   ttl    = 300
 }
 
-resource "digitalocean_droplet" "machine2" {
+resource "digitalocean_droplet" "server2" {
   image    = "centos-7-x64"
-  name     = "machine2.meinit.nl"
+  name     = "server2.meinit.nl"
   region   = "ams3"
   size     = "4gb"
   ssh_keys = ["${digitalocean_ssh_key.default.id}"]
 }
 
-resource "cloudflare_record" "machine2" {
+resource "cloudflare_record" "server2" {
   domain = "meinit.nl"
-  name   = "machine2"
-  value  = "${digitalocean_droplet.machine2.ipv4_address}"
+  name   = "server2"
+  value  = "${digitalocean_droplet.server2.ipv4_address}"
   type   = "A"
   ttl    = 300
 }
