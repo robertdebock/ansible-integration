3208311a5102027f1e3c43a7bae465777f3ec581
Robert de Bock <robert@meinit.nl>
2020-06-18 13:10:43 +0200

    Simpler and remove qouting.

---
+++ terraform/digitalocean.tf
@@ -0,0 +1,26 @@
+variable "do_token" {}
+
+provider "digitalocean" {
+  version = "~> 1.20"
+  token = var.do_token
+}
+
+resource "digitalocean_ssh_key" "default" {
+  name       = "integration"
+  public_key = file("id_rsa.pub")
+}
+resource "digitalocean_droplet" "server1" {
+  image    = "centos-7-x64"
+  name     = "server1.meinit.nl"
+  region   = "ams3"
+  size     = "2gb"
+  ssh_keys = [digitalocean_ssh_key.default.id]
+}
+
+resource "digitalocean_droplet" "server2" {
+  image    = "centos-7-x64"
+  name     = "server2.meinit.nl"
+  region   = "ams3"
+  size     = "4gb"
+  ssh_keys = [digitalocean_ssh_key.default.id]
+}
