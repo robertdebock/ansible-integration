9a8683b99ad7d3ad24179016890d940cf3c50abd
Robert de Bock <robert@meinit.nl>
2020-06-18 11:50:07 +0200

    Interpolation-only expressions are deprecated

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,7 +2,7 @@ variable "do_token" {}
 
 provider "digitalocean" {
   version = "~> 1.18"
-  token = "${var.do_token}"
+  token = "var.do_token"
 }
 
 variable "cloudflare_email" {}
@@ -10,6 +10,6 @@ variable "cloudflare_token" {}
 
 provider "cloudflare" {
   version = "~> 2.0"
-  email = "${var.cloudflare_email}"
-  api_key = "${var.cloudflare_token}"
+  email = "var.cloudflare_email"
+  api_key = "var.cloudflare_token"
 }
