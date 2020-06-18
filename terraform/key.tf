521ff70b180deac3511bfc2e85f26f92866db231
Robert de Bock <robert@meinit.nl>
2020-06-18 13:12:38 +0200

    Remove duplicate code.

--- terraform/key.tf
+++
@@ -1,4 +0,0 @@
-resource "digitalocean_ssh_key" "default" {
-  name       = "integration"
-  public_key = file("id_rsa.pub")
-}
--- terraform/providers.tf
+++
@@ -1,6 +0,0 @@
-variable "do_token" {}
-
-provider "digitalocean" {
-  version = "~> 1.20"
-  token = var.do_token
-}
