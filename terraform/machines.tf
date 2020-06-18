99d574379a0c890da8ac644fd71a382e37758282
Robert de Bock <robert@meinit.nl>
2020-06-18 12:41:14 +0200

    No records.

--- terraform/machines.tf
+++ terraform/machines.tf
@@ -1,7 +1,3 @@
-resource "digitalocean_domain" "default" {
-  name = "meinit.nl"
-}
-
 resource "digitalocean_droplet" "server1" {
   image    = "centos-7-x64"
   name     = "server1.meinit.nl"
@@ -10,26 +6,10 @@ resource "digitalocean_droplet" "server1" {
   ssh_keys = ["digitalocean_ssh_key.default.id"]
 }
 
-resource "cloudflare_record" "server1" {
-  domain = digitalocean_domain.default.name
-  name   = "server1"
-  value  = "digitalocean_droplet.server1.ipv4_address"
-  type   = "A"
-  ttl    = 300
-}
-
 resource "digitalocean_droplet" "server2" {
   image    = "centos-7-x64"
   name     = "server2.meinit.nl"
   region   = "ams3"
   size     = "4gb"
   ssh_keys = ["digitalocean_ssh_key.default.id"]
-}
-
-resource "cloudflare_record" "server2" {
-  domain = digitalocean_domain.default.name
-  name   = "server2"
-  value  = "digitalocean_droplet.server2.ipv4_address"
-  type   = "A"
-  ttl    = 300
 }
