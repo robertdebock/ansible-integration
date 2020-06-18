f716d142e4302b29ff9305332ad58946777ebfc8
Robert de Bock <robert@meinit.nl>
2020-06-18 13:01:19 +0200

    Remove unused provider.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,12 +4,3 @@ provider "digitalocean" {
   version = "~> 1.20"
   token = var.do_token
 }
-
-variable "cloudflare_email" {}
-variable "cloudflare_token" {}
-
-provider "cloudflare" {
-  version = "~> 2.7"
-  email = var.cloudflare_email
-  api_key = var.cloudflare_token
-}
